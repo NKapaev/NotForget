a2ecad1379caac6fab47adc13fd95617999ea9e6
Nikita Kapaev <nikita.kapaev@gmail.com>
2025-09-03 21:35:32 +0300

    Adds error page component

---
+++ src/components/pages/errorPage/ErrorPage.jsx
@@ -0,0 +1,24 @@
+import "./errorPage.css"
+import { useLocation, useNavigate } from 'react-router-dom'
+
+import Button from "../../ui/button/Button"
+
+export default function ErrorPage() {
+
+    const navigate = useNavigate()
+    const location = useLocation()
+    const error = location.state || {}
+
+    return (
+        <>
+            <svg width='100px' height="100px">
+                <use href="/smallLogo.svg#smallLogo" width="100px" height="100px" fill="#313ab1"></use>
+            </svg>
+            <p className="error-code">{error.code}</p>
+            <p>{error.message}</p>
+            <Button onClick={() => {
+                navigate("/")
+            }}>Back to main page</Button>
+        </>
+    )
+}
