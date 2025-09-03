b893f16640b41ccf4526d3d029e06b2b3aa760b9
Nikita Kapaev <nikita.kapaev@gmail.com>
2025-09-03 21:37:12 +0300

    Adds AddTile component

---
+++ src/components/addTile/AddTile.jsx
@@ -0,0 +1,52 @@
+import Form from "../form/Form"
+import useAddFolder from "../../hooks/useAddFolder"
+import useAddNote from "../../hooks/useAddNote"
+import { useModal } from "../../context/ModalProvider"
+
+export default function AddTile({ entity, folderId }) {
+    const { openModal, closeModal } = useModal()
+    const addFolder = useAddFolder()
+    const addNote = useAddNote()
+
+    const handleSubmit = (formData) => {
+        if (entity === "folder") {
+            addFolder.mutateAsync({ name: formData.name, description: formData.description })
+            closeModal()
+        }
+        if (entity === "note") {
+            addNote.mutateAsync({ content: formData.note, folderId })
+            closeModal()
+        }
+    }
+
+
+    if (entity === "folder") {
+        return (
+            <li className="addFolder tile" onClick={(e) => {
+                if (e.detail === 2) {
+                    e.preventDefault()
+                    openModal(<Form buttonText="Create" onSubmit={handleSubmit} fields={[{ name: "name", type: "text", placeholder: "Folder name" }, { name: "description", type: "text", placeholder: "Folder description" }]} />)
+                }
+            }}>
+                <svg className="svg" width="100px" height="100px">
+                    <use className="use" href="/icons/plus-icon.svg#plus" width="100px" height="100px" color="var(--blue)" fill="var(--blue)" stroke="#000"></use>
+                </svg>
+            </li>
+        )
+    }
+
+    if (entity === "note") {
+        return (
+            <li className="addFolder tile" onClick={(e) => {
+                if (e.detail === 2) {
+                    e.preventDefault()
+                    openModal(<Form buttonText="Create" onSubmit={handleSubmit} fields={[{ name: "note", type: "text", placeholder: "Enter note" }]} />)
+                }
+            }}>
+                <svg className="svg" width="100px" height="100px">
+                    <use className="use" href="/icons/plus-icon.svg#plus" width="100px" height="100px" color="var(--blue)" fill="var(--blue)" stroke="#000"></use>
+                </svg>
+            </li>
+        )
+    }
+}
