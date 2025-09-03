6ce1161602c4621f6fd8bf19d0aa006bbe7412bc
Nikita Kapaev <nikita.kapaev@gmail.com>
2025-09-03 21:33:54 +0300

    Rewrite Profile component

--- src/components/pages/profiile/Profile.jsx
+++ src/components/pages/profiile/Profile.jsx
@@ -1,12 +1,21 @@
+import "./profile.css"
+
 import { useParams, useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from "react-redux"
 import supabase from '../../../utils/supabase'
 
 import Header from '../../header/Header'
-import logOut from '../../../utils/logout'
+import TaskList from "../../taskList/TaskList"
+import FolderList from '../../folderList/FolderList'
+import NoteList from '../../noteList/NoteList'
+import { toggleTaskListOpen } from "../../redux/taskListSlice"
 
-export default function UserProfile() {
-    const { id } = useParams()
+export default function Profile() {
+    const dispatch = useDispatch()
+    const taskList = useSelector((state) => state.taskList)
+    console.log(taskList)
+    const { id, folderId } = useParams()
     const [profile, setProfile] = useState(null)
     const navigate = useNavigate()
 
@@ -20,8 +29,7 @@ export default function UserProfile() {
 
             if (error) {
                 navigate('/error', {
-                    state:
-                        { code: error.code, message: "Ошибка при загрузке профиля" }
+                    state: { code: error.code, message: 'Ошибка при загрузке профиля' },
                 })
                 return
             }
@@ -33,13 +41,25 @@ export default function UserProfile() {
     }, [id, navigate])
 
     if (!profile) return <p>Загрузка...</p>
-    return (
-        <section className='profile'>
 
+    return (
+        <section className="profile">
             <Header userData={profile} />
 
 
+            <div className="container profile-container">
+                <TaskList className={taskList.taskListShown ? "open" : ""}></TaskList>
 
-        </section >
+                <button onClick={(e) => {
+                    dispatch(toggleTaskListOpen())
+                }}>Открыть таск лист</button>
+
+                {folderId ? (
+                    <NoteList folderId={folderId} userId={profile.id} />
+                ) : (
+                    <FolderList userId={profile.id} />
+                )}
+            </div>
+        </section>
     )
 }
