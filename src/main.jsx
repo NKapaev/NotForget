12490c246dc74fb36c4e90385f4863232a3afd1a
Nikita Kapaev <nikita.kapaev@gmail.com>
2025-09-03 21:11:39 +0300

    Adds tanstak query

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { store } from './components/redux/store.js'
+import { Provider } from 'react-redux'
+
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import ModalProvider from './context/ModalProvider.jsx'
+
 import './index.css'
 import App from './App.jsx'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ModalProvider>
+          <App />
+        </ModalProvider>
+      </Provider>
+    </QueryClientProvider>
+  </StrictMode >
 )
