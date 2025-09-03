a49813f0960111e5403106c83ad5fa356f141031
Nikita Kapaev <nikita.kapaev@gmail.com>
2025-09-03 21:36:40 +0300

    Adds Email confirmation pages

---
+++ src/components/pages/confirmEmail/ConfirmEmail.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import supabase from '../../../utils/supabase'
+
+export default function ConfirmEmail() {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        // слушаем изменения авторизации
+        const { data: subscription } = supabase.auth.onAuthStateChange(
+            (_event, session) => {
+                if (session?.user) {
+                    navigate(`/profile/${session.user.id}`)
+                }
+            }
+        )
+
+        return () => {
+            subscription.subscription.unsubscribe()
+        }
+    }, [navigate])
+
+    return (
+        <section className="confirm-email-section">
+            <div className="container">
+                <h1>Подтвердите email</h1>
+                <p>
+                    Мы отправили письмо с подтверждением на вашу почту. <br />
+                    Пожалуйста, перейдите по ссылке в письме, чтобы активировать аккаунт.
+                </p>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/pages/emailConfirmed/EmailConfirmed.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+export default function EmailConfirmed() {
+    return (
+        <>
+            <h2>Email confirmed</h2>
+            <Link to={'http://localhost:3000'}>Back on main page</Link>
+        </>
+    )
+}
