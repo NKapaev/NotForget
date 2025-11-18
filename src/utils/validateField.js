eafb16d804e5fc766dad0e4d837f1ebfaec9851e
Nikita Kapaev <nikita.kapaev@gmail.com>
2025-11-18 05:41:49 +0200

    Rewrite errors mesages language

--- src/utils/validateField.js
+++ src/utils/validateField.js
@@ -1,21 +1,21 @@
 export default function validateField(name, value, allValues) {
     switch (name) {
         case "email":
-            if (!value) return "Email обязателен";
-            if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) return "Некорректный email";
+            if (!value) return "Email обов'язковий";
+            if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) return "некоректний email";
             return "";
 
         case "password":
-            if (value.length < 6) return "Пароль слишком короткий";
+            if (value.length < 6) return "Пароль надто короткий";
             // Если уже есть confirmPassword, проверим сразу
             if (allValues.confirmPassword && allValues.confirmPassword !== value) {
-                return "Пароли не совпадают";
+                return "Паролі не збігаються";
             }
             return "";
 
         case "confirmPassword":
-            if (!value) return "Повторите пароль";
-            if (value !== allValues.password) return "Пароли не совпадают";
+            if (!value) return "Повторіть пароль";
+            if (value !== allValues.password) return "Паролі не збігаються";
             return "";
 
         default:
