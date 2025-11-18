754a4a945848de4bf8d88748221a27e042a04471
Nikita Kapaev <nikita.kapaev@gmail.com>
2025-11-18 05:53:22 +0200

    Fix drop effect

--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -1,15 +1,19 @@
 import "./taskList.css"
 import { useQueryClient, useMutation } from "@tanstack/react-query"
 import { useState } from "react"
-import TaskExecution from "../taskExecution/TaskExecution"
-import Button from "../ui/button/Button"
 import supabase from "../../utils/supabase"
 import useNotes from "../../hooks/useNotes"
 import useDeleteNote from "../../hooks/useDeleteNote"
+import useDeleteTaskList from "../../hooks/useDeleteTaskList"
+
+import TaskExecution from "../taskExecution/TaskExecution"
+import Button from "../ui/button/Button"
+import Loader from "../ui/loader/Loader"
 
 export default function TaskList({ id, className, name }) {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(true)
     const queryClient = useQueryClient()
+    const deleteTaskList = useDeleteTaskList()
     const { data: notes, isLoading, error } = useNotes(null, id)
 
     const moveNoteMutation = useMutation({
@@ -39,7 +43,7 @@ export default function TaskList({ id, className, name }) {
         moveNoteMutation.mutate({ noteId, taskListId: id })
     }
 
-    if (isLoading) return <div>Загрузка...</div>
+    if (isLoading) return <Loader />
     if (error) return <div>Ошибка: {error.message}</div>
 
     return (
@@ -49,17 +53,34 @@ export default function TaskList({ id, className, name }) {
             onDrop={handleDrop}
             onDragOver={(e) => e.preventDefault()}
         >
-            <p>{name}</p>
+            <div className="task-list-header">
+                <p>{name}</p>
+                <Button
+                    className="delete-button"
+                    onClick={() => deleteTaskList.mutateAsync({ id })}
+                >
+                    <img
+                        width="40px"
+                        className="delete-button-icon"
+                        src="/icons/trash-icon.svg#trash-icon"
+                        alt=""
+                    />
+                </Button>
+            </div>
 
             {/* Проверяем, что notes не пустой массив */}
 
             {isOpen && notes?.length ? (
                 notes.map((note) => (
-                    <div key={note.id} className="taskList-item" draggable="true" onDragStart={(e) => {
+                    <div key={note.id} className="task-list-item" draggable="true" onDragStart={(e) => {
                         e.dataTransfer.setData("text/plain", note.id)
                     }}
                         onDragOver={(e) => { e.currentTarget.style.transform = "scale(1.2)" }}
                         onDragLeave={(e) => { e.currentTarget.style.transform = "scale(1)" }}
+
+                        onDrop={(e) => {
+                            e.currentTarget.style.transform = "scale(1)"
+                        }}
                     >
                         <Button
                             className="delete-button"
