3674240413fe580ed636c32a21b9fef906acb0db
Nikita Kapaev <nikita.kapaev@gmail.com>
2026-01-06 04:09:15 +0200

    Refactoring conditions

--- src/components/addTile/AddTile.jsx
+++ src/components/addTile/AddTile.jsx
@@ -1,49 +1,29 @@
-// import useAddFolder from "../../hooks/useAddFolder"
-// import useAddNote from "../../hooks/useAddNote"
-// import { useModal } from "../../context/ModalProvider"
-
-// import Form from "../form/Form"
-// import Button from "../ui/button/Button"
 import { useDispatch } from "react-redux"
 import { openModal } from "../redux/modalsSlice"
 
-
 export default function AddTile({ entity, folderId }) {
-    // const { openModal, closeModal } = useModal()
-    // const addFolder = useAddFolder()
-    // const addNote = useAddNote()
 
     const dispatch = useDispatch();
 
+    const handleClick = (e) => {
+        e.preventDefault()
 
+        const modalPayload = {
+            type: 'create', entity, modalId: crypto.randomUUID()
+        }
 
-    if (entity === "folder") {
-        return (
-            <li className="addFolder tile" onClick={(e) => {
+        if (entity === 'note') {
+            modalPayload.folderId = folderId
+        }
 
-                e.preventDefault()
-                dispatch(openModal({ type: 'create', entity: 'folder', id: crypto.randomUUID() }))
-
-            }}>
-                <svg className="svg" width="100px" height="100px">
-                    <use className="use" href="/icons/plus-icon.svg#plus" width="100px" height="100px" color="var(--blue)" fill="var(--blue)" stroke="#000"></use>
-                </svg>
-            </li>
-        )
+        dispatch(openModal(modalPayload))
     }
 
-    if (entity === "note") {
-        return (
-            <li className="addFolder tile" onClick={(e) => {
-
-                e.preventDefault()
-                dispatch(openModal({ type: 'create', entity: 'note', folderId, id: crypto.randomUUID() }))
-
-            }}>
-                <svg className="svg" width="100px" height="100px">
-                    <use className="use" href="/icons/plus-icon.svg#plus" width="100px" height="100px" color="var(--blue)" fill="var(--blue)" stroke="#000"></use>
-                </svg>
-            </li>
-        )
-    }
+    return (
+        <li className="addFolder tile" onClick={handleClick}>
+            <svg className="svg" width="100px" height="100px">
+                <use className="use" href="/icons/plus-icon.svg#plus" width="100px" height="100px" color="var(--blue)" fill="var(--blue)" stroke="#000"></use>
+            </svg>
+        </li>
+    )
 }
