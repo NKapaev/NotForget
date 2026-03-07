89b4b1fdc9ffeab590633d57316457cb37e12bdc
Nikita Kapaev <nikita.kapaev@gmail.com>
2026-03-07 16:45:00 +0200

    Adds className property for component

--- src/components/ui/loader/Loader.jsx
+++ src/components/ui/loader/Loader.jsx
@@ -1,8 +1,8 @@
 import styles from "./loader.module.css"
 
-export default function Loader({ variant }) {
+export default function Loader({ variant, className }) {
     return (
-        <div className={` ${variant === "big" ? styles.big : styles.loader}`}>
+        <div className={` ${variant === "big" ? styles.big : styles.loader} ${className ? className : ""}`}>
             <svg className={styles.loaderIcon}>
                 <use href="/icons/loader.svg"></use>
             </svg>
