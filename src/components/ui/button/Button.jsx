534b4b6a467023cceec7594ef2b5748f4c38d9bd
Nikita Kapaev <nikita.kapaev@gmail.com>
2026-03-07 16:50:30 +0200

    Adds disabling button functional in component

--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -1,11 +1,12 @@
 import styles from "./button.module.css"
 
-export default function Button({ className, type = "button", variant = "default", onClick, children, style }) {
+export default function Button({ className, disabled = false, type = "button", variant = "default", onClick, children, style }) {
     return (
         <button
             type={type}
-            className={`${styles.button} ${variant === "link" ? styles.link : ""} ${className ? className : ""}`}
+            className={`${styles.button} ${variant === "link" ? styles.link : ""} ${variant === "transparent" ? styles.transparent : ""} ${className ? className : ""}`}
             onClick={onClick}
+            disabled={disabled}
             style={style}
         >
             {children}
