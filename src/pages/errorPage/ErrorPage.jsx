15e9c720c0fed75811b1182a56ce75d54447a2e1
Nikita Kapaev <nikita.kapaev@gmail.com>
2026-06-10 22:23:56 +0300

    Change warning icon size

--- src/pages/errorPage/ErrorPage.jsx
+++ src/pages/errorPage/ErrorPage.jsx
@@ -16,7 +16,7 @@ export default function ErrorPage() {
                     <use href="/smallLogo.svg#smallLogo" width="100px" height="100px" fill="#313ab1"></use>
                 </svg>
                 <svg className={styles.warningIcon}>
-                    <use href="/icons/warning.svg"></use>
+                    <use href="/icons/warning.svg" width={"55px"} height={"55px"}></use>
                 </svg>
             </div>
             <p className={styles.errorMessage}>{error.message}</p>
